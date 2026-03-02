7c7324e72897f7be09523fe95d7ff1fc12fbae8e
Associe Data Analyst <associedataanalyst@gmail.com>
2026-03-02 14:33:15 +0700

    bug build

--- test-error.ts
+++
@@ -1,9 +0,0 @@
-
-const id = '123'; 
-const res = await fetch('http://localhost:3000/api/purchase-orders/'+id+'/receive', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ receivedAt: new Date().toISOString(), suratJalanUrl: 'test' })
-});
-console.log(await res.json());
-
